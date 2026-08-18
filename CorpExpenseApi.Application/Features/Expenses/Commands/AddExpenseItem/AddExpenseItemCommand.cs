d923b84c00e0d436592144f894f35c0b8787da2a
Pablo <pabloinfortech@gmail.com>
2026-08-18 19:28:05 -0400

    feat: creacion de command y handler para insertar ExpenseItem

---
+++ CorpExpenseApi.Application/Features/Expenses/Commands/AddExpenseItem/AddExpenseItemCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace CorpExpenseApi.Application.Features.Expenses.Commands.AddExpenseItem;
+
+public record AddExpenseItemCommand(
+        Guid ExpenseId,
+        string Description,
+        decimal Amount,
+        string Category
+    ) : IRequest;
---
+++ CorpExpenseApi.Application/Features/Expenses/Commands/AddExpenseItem/AddExpenseItemCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using CorpExpenseApi.Application.Interfaces;
+using CorpExpenseApi.Domain.Exceptions;
+using MediatR;
+
+namespace CorpExpenseApi.Application.Features.Expenses.Commands.AddExpenseItem;
+
+public class AddExpenseItemCommandHandler : IRequestHandler<AddExpenseItemCommand>
+{
+    private readonly IExpenseRepository _expenseRepository;
+
+    public AddExpenseItemCommandHandler(IExpenseRepository expenseRepository)
+    {
+        _expenseRepository = expenseRepository;
+    }
+
+    public async Task Handle(AddExpenseItemCommand request, CancellationToken cancellationToken)
+    {
+        var expense = await _expenseRepository.GetByIdAsync(request.ExpenseId);
+
+        if (expense == null)
+            throw new DomainException($"No se encontró el gasto con ID {request.ExpenseId}");
+        
+        expense.AddItem(request.Description, request.Amount, request.Category);
+        
+        await _expenseRepository.UpdateAsync(expense);
+    }
+}
