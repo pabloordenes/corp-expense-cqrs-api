b47658735b22bdbb897ffde9baf211624eee9f50
Pablo <pabloinfortech@gmail.com>
2026-08-18 19:18:24 -0400

    feat: creacion de command y handler para orquestar un "expense"

---
+++ CorpExpenseApi.Application/Features/Expenses/Commands/CreateExpense/CreateExpenseCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace CorpExpenseApi.Application.Features.Expenses.Commands.CreateExpense;
+
+public record CreateExpenseCommand(
+    string Description,
+    string Currency,
+    DateTime DateIncurred
+    ) : IRequest<Guid>;
---
+++ CorpExpenseApi.Application/Features/Expenses/Commands/CreateExpense/CreateExpenseCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection.Metadata;
+using CorpExpenseApi.Application.Interfaces;
+using CorpExpenseApi.Domain.Entities;
+using MediatR;
+
+namespace CorpExpenseApi.Application.Features.Expenses.Commands.CreateExpense;
+
+public class CreateExpenseCommandHandler : IRequestHandler<CreateExpenseCommand, Guid>
+{
+    private readonly IExpenseRepository _expenseRepository;
+
+    public CreateExpenseCommandHandler(IExpenseRepository expenseRepository)
+    {
+        _expenseRepository = expenseRepository;
+    }
+
+    public async Task<Guid> Handle(CreateExpenseCommand request, CancellationToken cancellationToken)
+    {
+        var newExpense = Expense.Create(
+            request.Description,
+            request.Currency,
+            request.DateIncurred
+            );
+
+        await _expenseRepository.AddAsync(newExpense, cancellationToken);
+        
+        return newExpense.Id;
+    }
+}
