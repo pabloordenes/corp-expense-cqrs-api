5e221fc2eb1ad34b026f63ce35882fc28f48eafa
Pablo <pabloinfortech@gmail.com>
2026-08-19 11:18:02 -0400

    fix: configuracion 1+N para items en Expense mediante configuracion de comportamiento + config total amount

--- CorpExpenseApi.Infrastructure/Persistence/Configuration/ExpenseConfiguration.cs
+++ CorpExpenseApi.Infrastructure/Persistence/Configuration/ExpenseConfiguration.cs
@@ -12,16 +12,21 @@ namespace CorpExpenseApi.Infrastructure.Persistence.Configuration
         public void Configure(EntityTypeBuilder<Expense> builder)
         {
             builder.ToTable("Expenses");
-
             builder.HasKey(e => e.Id);
 
             builder.Property(e => e.Description)
                 .IsRequired()
                 .HasMaxLength(250);
 
-            builder.Property(e => e.TotalAmount)
-                .HasPrecision(18, 2)
-                .IsRequired();
+            var navigation = builder.Metadata.FindNavigation(nameof(Expense.Items));
+            navigation.SetPropertyAccessMode(PropertyAccessMode.Field);
+
+            builder.HasMany(e => e.Items)
+                .WithOne()
+                .HasForeignKey("ExpenseId")
+                .OnDelete(DeleteBehavior.Cascade);
+            
+            builder.Ignore(e => e.TotalAmount);
 
             builder.Property(e => e.Currency)
                 .IsRequired()
