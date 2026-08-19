be7fd3c6c94b07f37b3c1e73dc98184c72dd6af2
Pablo <pabloinfortech@gmail.com>
2026-08-19 12:47:12 -0400

    feat: api controller Expense

---
+++ CorpExpenseApi.Api/Controllers/ExpenseController.cs
@@ -0,0 +1,42 @@
+﻿using CorpExpenseApi.Application.Features.Expenses.Commands.AddExpenseItem;
+using CorpExpenseApi.Application.Features.Expenses.Commands.CreateExpense;
+using CorpExpenseApi.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CorpExpenseApi.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ExpenseController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public ExpenseController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateExpense([FromBody] CreateExpenseCommand command,
+        CancellationToken cancellationToken)
+    {
+        var expenseId = await _mediator.Send(command, cancellationToken);
+        return Ok(new { Id = expenseId });
+    }
+
+    [HttpPost("{id}/items")]
+    public async Task<IActionResult> AddItem(Guid id, [FromBody] AddItemRequest request,
+        CancellationToken cancellationToken)
+    {
+        var command = new AddExpenseItemCommand(id, request.Description, request.Amount, request.Category);
+        
+        await _mediator.Send(command, cancellationToken);
+        
+        return Ok(new {Message = "Línea añadida con éxito."});
+    }
+
+    public record AddItemRequest(string Description, decimal Amount, string Category);
+}
+
+
