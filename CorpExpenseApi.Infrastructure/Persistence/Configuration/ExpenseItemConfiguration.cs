616cb8585a03762da6561ae9c31e800801eb545e
Pablo <pabloinfortech@gmail.com>
2026-08-24 17:50:37 -0400

    feat: config de entidad ExpenseItem en ef

---
+++ CorpExpenseApi.Infrastructure/Persistence/Configuration/ExpenseItemConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using CorpExpenseApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CorpExpenseApi.Infrastructure.Persistence.Configuration;
+
+public class ExpenseItemConfiguration : IEntityTypeConfiguration<ExpenseItem>
+{
+    public void Configure(EntityTypeBuilder<ExpenseItem> builder)
+    {
+        builder.ToTable("ExpenseItems");
+        
+        builder.HasKey(ei => ei.Id);
+
+        builder.Property(ei => ei.Amount)
+            .HasPrecision(18, 2)
+            .IsRequired();
+
+        builder.Property(ei => ei.Description)
+            .IsRequired()
+            .HasMaxLength(250);
+        
+        builder.Property(ei => ei.Category)
+            .IsRequired()
+            .HasMaxLength(100);
+    }
+}
